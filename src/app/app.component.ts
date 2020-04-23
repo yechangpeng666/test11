e8e243a6422f4be7f4a39a6908ac134f97876b4b
yechangpeng666 <2201427581@qq.com>
2020-04-23 17:45:36 +0800

    第十一周作业

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
+
+function userNameValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^a/)) {
+    return { invalidUser: true }
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Loginapp';
+  title(title: any) {
+    throw new Error("Method not implemented.");
+  }
+  myForm: FormGroup;//对应登录表单
+
+  userName: AbstractControl//输入用户名控件
+
+  password: AbstractControl
+
+  name$: Observable<string>
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      'userName': ['sss', Validators.compose([Validators.required, userNameValidator])],
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+    })
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password']
+    this.name$ = this.userName.valueChanges;
+  }
+  onSubmit(value: any) {
+    if (this.myForm.invalid) {
+      alert("表单无效")
+      return
+
+    }
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
